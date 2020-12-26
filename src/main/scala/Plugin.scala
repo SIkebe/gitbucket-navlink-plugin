2a0b9b0aa252abdf9eeb3a1b7464972f65067d3a
SIkebe <SIkebe@users.noreply.github.com>
2020-12-26 16:20:49 +0900

    v1.2.0

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -11,7 +11,8 @@ class Plugin extends gitbucket.core.plugin.Plugin with NavLinkSettingsService {
   override val versions: List[Version] = List(
     new Version("1.0.0"),
     new Version("1.0.1"),
-    new Version("1.1.0")
+    new Version("1.1.0"),
+    new Version("1.2.0")
   )
 
   override val controllers = Seq(
